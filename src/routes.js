b1cbe76f75991ebf5f0fffdd0b88901c76cfdd36
Arman <armvar774@gmail.com>
2020-08-22 14:27:15 +0400

    qwerty

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/pages/login'
-import { Router, Stack, Scene } from 'react-native-router-flux'
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Login from './src/pages/login'
+// import { Router, Stack, Scene } from 'react-native-router-flux'
 
-export default class Routes extends React.Component {
-    render(){
-  return (
-    <Router>
+// export default class Routes extends React.Component {
+//     render(){
+//   return (
+//     <Router>
         
-    </Router>
-  );
-}
+//     </Router>
+//   );
+// }
 
-}
+// }
